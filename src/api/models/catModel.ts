9a93716ece07a62ae7dd60516e841f732e77d939
lassib <91030337+lassib@users.noreply.github.com>
2023-09-08 18:27:46 +0300

    models done?

--- src/api/models/catModel.ts
+++ src/api/models/catModel.ts
@@ -1,1 +1,37 @@
 // TODO: mongoose schema for cat
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+export const catSchema = new Schema({
+  cat_name: {
+    type: String,
+  },
+  weight: {
+    type: Number,
+  },
+  filename: {
+    type: String,
+  },
+  birthdate: {
+    type: String,
+  },
+  location: {
+    type: {
+      type: String,
+    },
+    lat: {
+      type: Number,
+    },
+    long: {
+      type: Number,
+    },
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+const cat = mongoose.model('Cat', catSchema);
+module.exports = cat;
+export default cat;
--- src/api/models/userModel.ts
+++ src/api/models/userModel.ts
@@ -1,1 +1,23 @@
 // TODO: mongoose schema for user
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  user_name: String,
+  email: String,
+  role: String,
+  password: String,
+});
+
+userSchema.set('toJSON', {
+  transform: (doc: any, ret: typeof userSchema) => {
+    ret.id = ret._id.toString();
+    delete ret.__v;
+    delete ret.role;
+    delete ret.password;
+    delete doc.password;
+  },
+});
+
+const user = mongoose.model('User', userSchema);
+module.exports = user;
+export default user;
