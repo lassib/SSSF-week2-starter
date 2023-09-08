be9cf278c1516c149750b22abcf1ffd52783c6c5
lassib <91030337+lassib@users.noreply.github.com>
2023-09-08 18:11:22 +0300

    interfaces basic

--- src/interfaces/Cat.ts
+++ src/interfaces/Cat.ts
@@ -1,1 +1,18 @@
 // TODO: cat interface
+export interface Cat {
+  _id: any;
+  cat_name: string;
+  weight: number;
+  filename: string;
+  birthdate: string;
+  location: {
+    type: string;
+    lat: number;
+    long: number;
+  };
+  owner: {
+    owner_id: number;
+    user_name: string;
+    email: string;
+  };
+}
--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,1 +1,8 @@
 // TODO: user interface
+export interface User {
+  _id: any;
+  user_name: string;
+  email: string;
+  role: 'user' | 'admin';
+  password?: string;
+}
